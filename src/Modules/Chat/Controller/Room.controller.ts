d23f797ac57ea2d62d25a2338a5d614975915080
kamil <kamil.figarski@o2.pl>
2024-09-03 00:03:49 +0200

    add heart beat

--- src/Modules/Chat/Controller/Room.controller.ts
+++ src/Modules/Chat/Controller/Room.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Get, Post, Sse } from '@nestjs/common';
 import { RoomService } from '../Services/Room.service';
 import { Rooms } from '../Models/Rooms';
 import { CreateRoomPayload } from './CreateRoom.payload';
-import { fromEvent, interval, map, Observable } from 'rxjs';
+import { fromEvent, interval, map, merge, Observable, startWith } from 'rxjs';
 import { EventEmitter2 } from '@nestjs/event-emitter';
 
 @Controller('rooms')
@@ -26,13 +26,21 @@ export class RoomController {
 
   @Sse('room-created')
   roomCreated(): Observable<MessageEvent> {
-    return fromEvent(this.eventEmitter, 'room-created').pipe(
+    const eventStream = fromEvent(this.eventEmitter, 'room-created').pipe(
       map(
-        (_) =>
+        () =>
           new MessageEvent('room-created', {
             data: this.roomService.getRooms(),
           }),
       ),
+    );
+
+    const heartbeatStream = interval(15000).pipe(
+      map(() => new MessageEvent('heartbeat', { data: 'keep-alive' })),
+    );
+
+    return merge(eventStream, heartbeatStream).pipe(
+      startWith(new MessageEvent('heartbeat', { data: 'keep-alive' })),
     );
   }
 }
